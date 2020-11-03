d6deba0e09414ce0c6e461ba6526a8d5e09e4aab
Idriss Kone <idriss.kone@icloud.com>
2020-11-02 19:03:08 -0500

    added animated svgs

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const wind = document.querySelector("#wind");
 const weatherIcon = document.querySelector("#weather-icon");
 const autocompleteItem = document.querySelector("#autocomplete-item");
 const weatherDescription = document.querySelector("h3");
+const forecastBtn = document.querySelector("#forecast-btn");
+const currentContent = document.querySelector(".main-content-current");
 
 const celcius = String.fromCharCode(8451);
 
@@ -33,22 +35,27 @@ axios({
 search.addEventListener("submit", (e) => {
   e.preventDefault();
   e.stopImmediatePropagation();
-  //   const city = e.target[0].value;
-  //   let URL = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}`;
+  const city = e.target[0].value;
+  let URL = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}`;
 
-  //   axios
-  //     .get(
-  //       `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKEY}`
-  //     )
-  //     .then((res) => {
-  //       cityName.textContent = res.data.name;
-  //       currentTemp.textContent = `${res.data.main.temp} ${celcius} `;
-  //       minTemp.textContent = `${res.data.main.temp_min} ${celcius}`;
-  //       maxTemp.textContent = `${res.data.main.temp_max} ${celcius}`;
-  //       atmosPressure.textContent = `${res.data.main.pressure} hPa`;
-  //       humidity.textContent = `${res.data.main.humidity}%`;
-  //       wind.textContent = `${res.data.wind.speed} km/h, SW`;
-  //       weatherIcon.src = `./icons/${res.data.weather[0].icon}.png`;
-  //       console.log(res.data);
-  //     });
+  axios
+    .get(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKEY}`
+    )
+    .then((res) => {
+      cityName.textContent = res.data.name;
+      currentTemp.textContent = `${res.data.main.temp} ${celcius} `;
+      minTemp.textContent = `${res.data.main.temp_min} ${celcius}`;
+      maxTemp.textContent = `${res.data.main.temp_max} ${celcius}`;
+      atmosPressure.textContent = `${res.data.main.pressure} hPa`;
+      humidity.textContent = `${res.data.main.humidity}%`;
+      wind.textContent = `${res.data.wind.speed} km/h, SW`;
+      weatherIcon.src = `./icons/${res.data.weather[0].icon}.png`;
+      console.log(res.data);
+    });
+});
+
+forecastBtn.addEventListener("click", () => {
+  currentContent.style.display = "none";
+  forecastBtn.classList.add("selected");
 });
